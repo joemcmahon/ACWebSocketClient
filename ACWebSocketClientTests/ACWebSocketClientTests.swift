f1384a01e78ab6cbbabe5a25681ddb96d01d94cf
Joe McMahon <joe.mcmahon@gmail.com>
2024-12-26 10:10:05 -0800

    Initial Commit

---
+++ ACWebSocketClientTests/ACWebSocketClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  ACWebSocketClientTests.swift
+//  ACWebSocketClientTests
+//
+//  Created by Joe McMahon on 12/26/24.
+//
+
+import Testing
+@testable import ACWebSocketClient
+
+struct ACWebSocketClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
